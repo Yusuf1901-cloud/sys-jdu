cb1f7070a1e249ad30c4dee404609f4448a09ab7
yusuf <yusuf.t@jdu.uz>
2024-01-19 11:47:56 +0500

    Added djazzmin admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import JduUser
 
 # Register your models here.
+@admin.register(JduUser)
+class JduUserAdmin(admin.ModelAdmin):
+    list_display = ('jdu_id', 'name', 'surname', 'exam_score')
+    list_filter = ('jdu_id', 'exam_score')
+    search_fields = ['jdu_id', 'jdu_name', 'phone_num', 'address']
